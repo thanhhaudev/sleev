1174feb194cca600ed6329c52cf7914fcb38aa00
thanhhaudev <thanhhaudev@gmail.com>
2026-05-17 16:17:26 +0700

    feat: CollapseLengthCalculator + Preferences with unit coverage

---
+++ SleevTests/CollapseLengthCalculatorTests.swift
@@ -0,0 +1,20 @@
+@testable import Sleev
+import XCTest
+
+final class CollapseLengthCalculatorTests: XCTestCase {
+    func test_collapseLengthIsBoundedByMinimum() {
+        XCTAssertEqual(CollapseLengthCalculator.collapseLength(forScreenWidth: 100), 500)
+    }
+
+    func test_collapseLengthIsBoundedByMaximum() {
+        XCTAssertEqual(CollapseLengthCalculator.collapseLength(forScreenWidth: 10000), 4000)
+    }
+
+    func test_collapseLengthUsesScreenWidthPlusBuffer() {
+        XCTAssertEqual(CollapseLengthCalculator.collapseLength(forScreenWidth: 1728), 1928)
+    }
+
+    func test_visibleSeparatorLength() {
+        XCTAssertEqual(CollapseLengthCalculator.visibleSeparatorLength, 20)
+    }
+}
